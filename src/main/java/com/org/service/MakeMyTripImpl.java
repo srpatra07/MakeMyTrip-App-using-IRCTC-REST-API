6ed54dba797095257603f8ff9edf57787e970584
Swarup Ranjan Patra <swarupranjan2424@gmail.com>
2024-06-20 12:17:56 +0530

    WebClient is implemented in place of RestTemplate to make as Consumer Production

--- src/main/java/com/org/service/MakeMyTripImpl.java
+++ src/main/java/com/org/service/MakeMyTripImpl.java
@@ -1,35 +1,58 @@
 package com.org.service;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.org.request.Passenger;
 import com.org.response.Ticket;
 
 @Service
 public class MakeMyTripImpl implements MakeMyTripService {
-	
+
 	private String BOOK_TICKET_URL = "http://localhost:8080/ticket";
-	
+
 	private String GET_TICKET_URL = "http://localhost:8080/ticket/{ticketNum}";
 
 	@Override
 	public Ticket bookTicket(Passenger passenger) {
-		
-		RestTemplate rt = new RestTemplate();
-		ResponseEntity<Ticket> resEntity = rt.postForEntity(BOOK_TICKET_URL, passenger, Ticket.class);
-		Ticket ticket = resEntity.getBody();
+
+		/* 
+		 * RestTemplate rt = new RestTemplate(); ResponseEntity<Ticket> resEntity =
+		 * rt.postForEntity(BOOK_TICKET_URL, passenger, Ticket.class); Ticket ticket =
+		 * resEntity.getBody(); return ticket;
+		 */
+
+		//Creating the Object of WebClient Implementation class
+		WebClient client = WebClient.create();
+		//Send POST Request with passenger data
+		//map response to Ticket Object
+		Ticket ticket = client.post()
+				.uri(BOOK_TICKET_URL)
+				.bodyValue(passenger)
+				.retrieve()
+				.bodyToMono(Ticket.class)
+				.block();
 		
 		return ticket;
 	}
 
 	@Override
 	public Ticket getTicket(Integer ticketNum) {
+
+		/*
+		 * RestTemplate rt = new RestTemplate(); ResponseEntity<Ticket> resEntity =
+		 * rt.getForEntity(GET_TICKET_URL, Ticket.class, ticketNum); Ticket ticket =
+		 * resEntity.getBody(); return ticket;
+		 */
 		
-		RestTemplate rt = new RestTemplate();
-		ResponseEntity<Ticket> resEntity = rt.getForEntity(GET_TICKET_URL, Ticket.class,ticketNum);
-		Ticket ticket = resEntity.getBody();
+		//Creating the instance of WebClient Implementation class
+		WebClient client = WebClient.create();
+		// send get request and map response to Ticket Obj
+		Ticket ticket = client.get()
+			.uri(GET_TICKET_URL,ticketNum)
+			.retrieve()
+			.bodyToMono(Ticket.class)
+			.block();
 		
 		return ticket;
 	}
